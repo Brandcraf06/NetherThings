c8021284c898927d58730c9922e67273e4513a9f
Draylar <samuelhimes@gmail.com>
2019-07-11 11:02:19 -0500

    major change version bump (1.1.2.1 -> 1.1.3.0)

--- src/main/java/com/brand/netherthings/NetherThings.java
+++ src/main/java/com/brand/netherthings/NetherThings.java
@@ -18,7 +18,7 @@ import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 public class NetherThings implements ModInitializer {
 	
 	public static final String MOD_ID = "netherthings";
-	public static final String VERSION = "1.1.2.1";
+	public static final String VERSION = "1.1.3.0";
 	public static final String NAME = "NetherThings";
 	public static final NetherThingsConfig CONFIG = AutoConfig.register(NetherThingsConfig.class, GsonConfigSerializer::new).getConfig();
 	
