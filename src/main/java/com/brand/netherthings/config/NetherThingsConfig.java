82b1188f6c8fb5dd8c25a2a5ab0e2561a61e1d76
Draylar <samuelhimes@gmail.com>
2019-07-11 10:54:06 -0500

    add config options for armor and tool materials

--- src/main/java/com/brand/netherthings/config/NetherThingsConfig.java
+++ src/main/java/com/brand/netherthings/config/NetherThingsConfig.java
@@ -10,4 +10,44 @@ public class NetherThingsConfig implements ConfigData {
     public boolean enableBasaltGeneration = true;
     public boolean enableNetherOresInOverworld = true;
     public boolean enableOverworldOresInNether = true;
+    
+    // vibranium config
+    public int vibraniumToolDurability = 21850;
+    public float vibraniumToolSpeed = 22f;
+    public float vibraniumToolDamage = 10f;
+    public int vibraniumToolEnchantability = 7;
+    public int vibraniumArmorDurabilityModifier = 1050;
+    public int[] vibraniumArmorProtectionAmounts = {
+            5,
+            9,
+            10,
+            5
+    };
+
+    // glowstone config
+    public int glowstoneToolDurability = 442;
+    public float glowstoneToolSpeed = 5f;
+    public float glowstoneToolDamage = 4.7f;
+    public int glowstoneToolEnchantability = 15;
+    public int glowstoneArmorDurabilityModifier = 27;
+    public int[] glowstoneArmorProtectionAmounts = {
+            2,
+            5,
+            6,
+            3
+    };
+    
+    // nether config
+    public int netherToolDurability = 280;
+    public float netherToolSpeed = 5f;
+    public float netherToolDamage = 3.5f;
+    public int netherToolEnchantability;
+    public int netherArmorDurabilityModifier = 16;
+    public int[] netherArmorProtectionAmounts = {
+            2,
+            5,
+            5,
+            2
+    };
+    
 }
